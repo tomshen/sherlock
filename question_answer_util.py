6a5320856c1b1d0815ad31f7b1a5a6cef3b9d1ed
Alexander M. Shoup <shoup_alex@yahoo.com>
2014-03-18 20:22:12 -0400

    whoops, let's try to compile

--- question_answer_util.py
+++ question_answer_util.py
@@ -8,19 +8,26 @@ import grammars
 import util
 import backup_answer
 
+Relations = util.enum('REL', 'ISA', 'HASA')
+
 def parse_question(q):
-    toks = nltk.word_tokenize(q)
-    toks[0] = toks[0].lower()
-    tags = nltk.pos_tag(toks)
-    cp = nltk.RegexpParser(grammars.noun_phrase)
-    tree = cp.parse(tags)
-    subject = tree_node_to_text(tree[1])
-    query = tree_node_to_text(tree[2])
-    relation = ''
-    return (subject, query, relation)
+    return ('', '', '')
+    #toks = nltk.word_tokenize(q)
+    #toks[0] = toks[0].lower()
+    #tags = nltk.pos_tag(toks)
+    #cp = nltk.RegexpParser(grammars.noun_phrase)
+    #tree = cp.parse(tags)
+    #subject = tree_node_to_text(tree[1])
+    #query = tree_node_to_text(tree[2])
+    #relation = ''
+    #return (subject, query, relation)
 
 def related(subject, query, relation, database):
-    if subject in database
+    return ''
+    if subject in database:
+        if query in database[subject]:
+            if relation == database[subject][query]["type"]:
+                if relation == Relations
 
 #ignore relation for now
     #return subject in database and query in database[subject]
