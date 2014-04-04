a7c281b0c7788f66b7590bce7a6a68ee5eaf438e
allie-wood <allisonwood94@gmail.com>
2014-04-04 17:09:53 -0400

    Asker function takes in parameters

--- asker.py
+++ asker.py
@@ -10,9 +10,16 @@ from nltk.corpus import wordnet as wn
 import string
 from textblob.taggers import NLTKTagger
 
-nltk_tagger = NLTKTagger()
 
-def ask_questions():
+def ask_questions(numQuestions, filename):
+    nltk_tagger = NLTKTagger()
+    p = inflect.engine()
+    try:
+        doc = util.load_article(filename)
+        database = parse.basic_parse(doc)
+    except:
+        print "Invalid filename: " + filename
+        sys.exit()
     art = ("the", "an", "a", "The")
     pairs = []
     result = []
@@ -34,7 +41,7 @@ def ask_questions():
         key_plural = util.is_plural(key)
         is_verb = "Are" if key_plural else "Is"
         verb_phrase = entry['relation']
-        print verb_phrase
+        #print verb_phrase
         try:
             verb = verb_phrase[0]
         except: verb = "related"
@@ -56,37 +63,30 @@ def ask_questions():
                 if tag[0] == "V":
                     verbs += [(w, tag)]
             action = [w for (w, tag) in verbs if tag != "VBZ"]
-            try:
-                w = Word(action[0])
-                question3 = "What does %s %s?" % (key,  w.lemmatize("v"))
-                print question3
-            except: pass
+            det_option = " the" if not key[0].isupper() else ""
+
+            for (w, tag) in verbs:
+                question3 = "What does%s %s %s?" % (det_option, key,  w.lemmatize("v"))
+                #print question3
             det_option = " the" if not key[0].isupper() else ""
             question2 = "%s%s %s %s %s?" % (is_verb, det_option, key, blob, value)
-            print question2
+            #print question2
             question = "%s%s %s related to %s?" % (is_verb, det_option, key, value)
         if (question != ""):
-            print question, entry['sentiment']
+            print question
             result += [question]
     return result
         
         
 if __name__ == "__main__":
-    p = inflect.engine()
 
     try:
         filename = sys.argv[1]
         numQuestions = int(sys.argv[2])
     except:
-        print "Invalid filename or numQuestions, using default values"
+        #print "Invalid filename or numQuestions, using default values"
         filename = 'set4/a1'
         numQuestions = 30
-    try:
-        doc = util.load_article(filename)
-        database = parse.basic_parse(doc)
-    except:
-        print "Invalid filename: " + filename
-        sys.exit()
-
-    print "Generating %d questions from %s" % (numQuestions, filename)
-    ask_questions()
+        
+    #print "Generating %d questions from %s" % (numQuestions, filename)
+    ask_questions(numQuestions, filename)
