44973818e847b9103c22f6c139663cd3a89dcbc8
Alexander M. Shoup <shoup_alex@yahoo.com>
2014-04-07 03:29:37 -0400

    renamed answer.py to answer

--- question_answer_util.py
+++ question_answer_util.py
@@ -4,12 +4,61 @@ import nltk
 import inflect
 import re
 import string
+from textblob import TextBlob
+from textblob.np_extractors import ConllExtractor
+extractor = ConllExtractor()
 
 import grammars as g
 import util
 import backup_answer as b
+import parse as p
 
 Relations = util.enum('REL', 'ISA', 'HASA')
+
+def parse_yn(q, database):
+    words = q.words
+    nps = q.noun_phrases
+    subj = nps[0] #assuming subject is the first noun phrase
+    first = words[0]
+
+    if first.lower() in ['is','was']: #is this everything?
+        #question is an "is/was ___ NP/AP"
+        #get index of the first word after the noun phrase
+        n = len(subj.split())
+        next = ""
+        nexti = 0
+        for i in xrange(0, len(words) - n - 1):
+            if subj == " ".join(words[i:i+n]):
+                next = words[i+n]
+                nexti = i+n
+                break
+        if next == "":
+            print >> sys.stderr, "something bad happened."
+        #is that part of its own noun phrase? (det. or noun)
+        nextword, nexttag = q.tags[nexti]
+        print >> sys.stderr, nextword, nexttag
+        nextnp = ""
+        for np in nps:
+            if np.start == nexti:
+                nextnp = np
+                break
+        if nextnp != "":
+            #if yes then look to see if they are related in the db
+            return "placeholder!"
+        #otherwise, assume it's an adjective phrase
+        else:
+            #rebuild sentence fragments from database
+            #compare to the AP
+            #if one matches, -> yes
+            #else -> no
+            return "No"
+    elif first.lower() in ['does','did','will']:
+        #question is a "does/did/will ___ VP"
+        return "No"
+    return "No"
+
+def parse_wh(q, database):
+    return ""
 
 def parse_question(q, database, raw):
     toks = nltk.word_tokenize(q)
@@ -41,19 +90,9 @@ def parse_question(q, database, raw):
     print >> sys.stderr, 'error parsing question, resorting to backup'
     return b.backup_answer(q, raw)
 
-#deprecated
-def related(subject, query, relation, database):
-    if subject in database:
-        if query in database[subject]:
-            if relation == database[subject][query]["type"]:
-                if relation == Relations.ISA:
-                    return subject + " is a " + query + "."
-                elif relation == Relations.HASA:
-                    return subject + " has a " + query + "."
-                else:
-                    return subject + " is related to " + query + "."
-    else:
-        return " ".join(["Database error on s/q/r:", subject, query, relation])
-
-#ignore relation for now
-    #return subject in database and query in database[subject]
+if __name__ == "__main__":
+    q = raw_input("Ask a question\n")
+    q = TextBlob(q, np_extractor=extractor)
+    print q.noun_phrases
+    print q.parse()
+    print p.extract_generic_relations(q)
